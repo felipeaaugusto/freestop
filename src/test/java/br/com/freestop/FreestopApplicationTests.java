91a546af397439d94ba18250c80730d259c7c17f
Felipe Albuquerque <felipeaugustobento@hotmail.com>
2020-04-10 20:09:06 -0300

    Configurando heroku

--- src/test/java/br/com/freestop/FreestopApplicationTests.java
+++ src/test/java/br/com/freestop/FreestopApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.freestop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FreestopApplicationTests {
 
 	@Test
