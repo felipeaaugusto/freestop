618ec86680ea2312172af18d5f07839d69a6acb0
Felipe Albuquerque <felipeaugustobento@hotmail.com>
2020-04-19 21:58:45 -0300

    Melhorias nas mensagens de validação/erro #2

--- src/main/java/br/com/freestop/exception/BadRequestException.java
+++ src/main/java/br/com/freestop/exception/BadRequestException.java
@@ -3,7 +3,7 @@ package br.com.freestop.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Bad Request.")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BadRequestException extends HttpStatusException {
 
 	private static final long serialVersionUID = 1L;
